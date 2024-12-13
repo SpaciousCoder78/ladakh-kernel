5e408026b2798c487e5d22f410abc4912523b00a
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-12-13 11:04:06 +0530

    created required files and folders

---
+++ boot/boot.s
@@ -0,0 +1,17 @@
+/*Assembly boot variables and bootstrap code for Ladakh Kernel
+File Name: boot.s
+Authors: Aryan Karamtoth
+Last Revision: 13 Dec 2024
+Version: 1.0
+License: GNU GPL 3.0
+*/
+
+/*constant declaration for multiboot header */
+
+.set ALIGN, 1<<0 /*align loaded modules on page boundaries */
+.set MEMINFO, 1<<1 /*provide memory map */
+.set FLAGS, ALIGN | MEMINFO /*multiboot flag field */
+.set MAGIC, 0x1BADB002 /* enables bootloader to find the header*/
+.set CHECKSUM, -(MAGIC + FLAGS) /*checksum to prove multiboot */
+
+
