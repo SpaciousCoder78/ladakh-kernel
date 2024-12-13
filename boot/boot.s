92900007c2cec49bf9b572e547ea47b819ed3ca6
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-12-13 12:00:55 +0530

    fixed files

--- boot/boot.s
+++ boot/boot.s
@@ -6,15 +6,15 @@
         License: GNU GPL 3.0
 */
 
-/*constant declaration for multiboot header */
-
-.set ALIGN, 1<<0 /*align loaded modules on page boundaries */
-.set MEMINFO, 1<<1 /*provide memory map */
-.set FLAGS, ALIGN | MEMINFO /*multiboot flag field */
-.set MAGIC, 0x1BADB002 /* enables bootloader to find the header*/
-.set CHECKSUM, -(MAGIC + FLAGS) /*checksum to prove multiboot */
+/* Declare constants for the multiboot header. */
+.set ALIGN,    1<<0             /* align loaded modules on page boundaries */
+.set MEMINFO,  1<<1             /* provide memory map */
+.set FLAGS,    ALIGN | MEMINFO  /* this is the Multiboot 'flag' field */
+.set MAGIC,    0x1BADB002       /* 'magic number' lets bootloader find the header */
+.set CHECKSUM, -(MAGIC + FLAGS) /* checksum of above, to prove we are multiboot */
 
 /*multiboot header to mark program as kernel */
+
 .section .multiboot
 .align 4
 .long MAGIC
@@ -24,27 +24,32 @@
 /*creating a small stack */
 .section .bss
 .align 16
-stack_botton:
+stack_bottom:
 .skip 16384 # 16 KiB
 stack_top:
 
 /*_start is definied as entry point once kernel is loaded */
+
 .section .text
 .global _start
 .type _start, @function
 _start:
+	
 
-/*To use stack set esp reg to point to top of stack */
-mov $stack_top, %esp
+	/*To use stack set esp reg to point to top of stack */
 
-/*Enter high level kernel */
+	mov $stack_top, %esp
 
-call kernel_main
+	/*Enter high level kernel */
 
-/*Disable interrupts with cli , wait for interrupt to arrive at hlt and jump to hlt when required */
-cli
-1: hlt
-   jmp 1b
+	call kernel_main
+
+	/*Disable interrupts with cli , wait for interrupt to arrive at hlt and jump to hlt when required */
+
+	cli
+1:	hlt
+	jmp 1b
 
 /*Set size of _start to current loc minus start, usefu for call tracing */
+
 .size _start, . - _start
